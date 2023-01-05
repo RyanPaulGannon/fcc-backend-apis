dee6c9b5703ae9c245536a43087b038798dd9779
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-01-05 15:42:33 +0000

    Removed comments

--- exercise-tracker/src/database/client.ts
+++ exercise-tracker/src/database/client.ts
@@ -3,10 +3,8 @@ import { PrismaClient } from "@prisma/client"
 let prisma = new PrismaClient()
 
 export async function connect() {
-  // Connect the client
   await prisma.$connect()
   console.log("Connected")
-  // ... you will write your Prisma Client queries here
 }
 
 export default prisma
