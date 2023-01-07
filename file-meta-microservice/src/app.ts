45d9066a0b3020dbc37c8c6c9c9a9df443f37065
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-01-07 17:08:30 +0000

    Update README, 3/4 done

--- file-meta-microservice/src/app.ts
+++ file-meta-microservice/src/app.ts
@@ -1,10 +1,12 @@
 import "dotenv/config"
 import cors from "cors"
+import multer from "multer"
 import express, { Application, Request, Response } from "express"
 
 /* config */
 const app: Application = express()
 const port = process.env.PORT || 4000
+const upload = multer({ dest: "uploads/" })
 
 /* cors */
 app.use(cors({ origin: "*" }))
@@ -15,6 +17,19 @@ app.use(express.urlencoded({ extended: false }))
 app.get("/", (req: Request, res: Response) => {
   res.sendFile(process.cwd() + "/src/views/index.html")
 })
+
+/*  */
+app.post(
+  "/api/fileanalyse",
+  upload.single("file"),
+  (req: Request, res: Response) => {
+    res.json({
+      name: req.body.filename,
+      type: req.headers["content-type"],
+      size: req.body.size,
+    })
+  }
+)
 
 /* listener */
 app.listen(port, () => console.log(`Node Server listening on port ${port}`))
