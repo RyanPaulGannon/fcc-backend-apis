9e200f1d9b38791edcde6acd3bb7de3f91e50f2e
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-03-27 22:58:23 +0100

    Added Dockerfile and pnpm

--- exercise-tracker/src/database/client.ts
+++ exercise-tracker/src/database/client.ts
@@ -1,10 +1,7 @@
 import { PrismaClient } from "@prisma/client"
 
-let prisma = new PrismaClient()
-
+export const prisma = new PrismaClient()
 export async function connect() {
   await prisma.$connect()
   console.log("Connected")
 }
-
-export default prisma
--- exercise-tracker/src/database/exerciseTracker.ts
+++ exercise-tracker/src/database/exerciseTracker.ts
@@ -1,4 +1,4 @@
-import prisma from "./client"
+import { prisma } from "./client"
 
 export function createUser(username: string) {
   return prisma.user.create({
